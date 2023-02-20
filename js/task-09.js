af045496ee6b6a6a4ab4ce0ec39328693a2d5a1f
KarinaLiashko <112810602+KarinaLiashko@users.noreply.github.com>
2023-02-20 19:43:13 +0100

    Update-task-9-js

--- js/task-09.js
+++ js/task-09.js
@@ -1,6 +1,10 @@
 const buttonEl = document.querySelector(".change-color");
 const textEl = document.querySelector(".color");
 
+const bodyStyles = getComputedStyle(document.documentElement);
+const bgColor = bodyStyles.getPropertyValue('background-color');
+document.body.style.backgroundColor = bgColor;
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
@@ -9,6 +13,6 @@ function getRandomHexColor() {
 
 buttonEl.addEventListener("click", () => {
   document.body.style.backgroundColor = getRandomHexColor();
-  textEl.textContent = document.body.style.backgroundColor;
+  textEl.textContent = bgColor;
 }); 
 
